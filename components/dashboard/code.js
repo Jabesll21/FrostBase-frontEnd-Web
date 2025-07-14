a4ba206a0f4d1a731deb9b85a2026174b902ffa1
SKSKronosUTT <0323105967@ut-tijuana.edu.mx>
2025-07-13 17:57:18 -0700

    Modified Dashboard to make it look like Torres one

--- components/dashboard/code.js
+++ components/dashboard/code.js
@@ -11,33 +11,32 @@ function getData() {
     var today = new Date()
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
     getTotals(date).then( (response) => {
-        console.log(response)
         //Change values
-        document.getElementById('label-active-sessions').textContent = response.activeSessions
-        document.getElementById('label-active-calls').textContent = response.activeCalls
-        document.getElementById('label-total-calls').textContent = response.totalCalls
-        document.getElementById('label-avg-handle-time').textContent = response.averageHandleTime.value
-        document.getElementById('label-wait-time').textContent = response.waitTime.value
-        document.getElementById('label-calls-in-queue').textContent = response.callsInQueue.value
+        document.getElementById('label-backlog').textContent = response.backlog
+        document.getElementById('label-process').textContent = response.process
+        document.getElementById('label-completed').textContent = response.completed
+        document.getElementById('label-alerts').textContent = response.alerts.value
+        document.getElementById('label-above').textContent = response.above.value
+        document.getElementById('label-below').textContent = response.below.value
         //Set colors
         //Calls in Queue
-        document.getElementById('div-avg-handle').style.background = 'var(--' + response.averageHandleTime.status + ')'
-        document.getElementById('icon-avg-handle').style.background = 'var(--' + response.averageHandleTime.status + ')'
-        document.getElementById('value-avg-handle').style.background = 'var(--' + response.averageHandleTime.status + ')'
-        document.getElementById('div-avg-handle-bottom').style.background = 'var(--' + response.averageHandleTime.status + 'Dark)'
+        document.getElementById('div-alerts').style.background = 'var(--' + response.alerts.status + ')'
+        document.getElementById('icon-alerts').style.background = 'var(--' + response.alerts.status + ')'
+        document.getElementById('value-alerts').style.background = 'var(--' + response.alerts.status + ')'
+        document.getElementById('div-alerts-bottom').style.background = 'var(--' + response.alerts.status + 'Dark)'
         //Calls in Queue
-        document.getElementById('div-wait-time').style.background = 'var(--' + response.waitTime.status + ')'
-        document.getElementById('icon-wait-time').style.background = 'var(--' + response.waitTime.status + ')'
-        document.getElementById('value-wait-time').style.background = 'var(--' + response.waitTime.status + ')'
-        document.getElementById('div-wait-time-bottom').style.background = 'var(--' + response.waitTime.status + 'Dark)'
+        document.getElementById('div-above').style.background = 'var(--' + response.above.status + ')'
+        document.getElementById('icon-above').style.background = 'var(--' + response.above.status + ')'
+        document.getElementById('value-above').style.background = 'var(--' + response.above.status + ')'
+        document.getElementById('div-above-bottom').style.background = 'var(--' + response.above.status + 'Dark)'
         //Calls in Queue
-        document.getElementById('div-calls-in-queue').style.background = 'var(--' + response.callsInQueue.status + ')'
-        document.getElementById('icon-calls-in-call').style.background = 'var(--' + response.callsInQueue.status + ')'
-        document.getElementById('value-calls-in-call').style.background = 'var(--' + response.callsInQueue.status + ')'
-        document.getElementById('div-calls-in-queue-bottom').style.background = 'var(--' + response.callsInQueue.status + 'Dark)'
+        document.getElementById('div-below').style.background = 'var(--' + response.below.status + ')'
+        document.getElementById('icon-below').style.background = 'var(--' + response.below.status + ')'
+        document.getElementById('value-below').style.background = 'var(--' + response.below.status + ')'
+        document.getElementById('div-below-bottom').style.background = 'var(--' + response.below.status + 'Dark)'
 
         //Draw chart
-        drawChart(response)
+        drawChart(response.records)
     })
     
     clearInterval(intervalData)
@@ -48,83 +47,54 @@ function getData() {
 //draw call duration chart
 function drawChart(data) {
     console.log('Drawing charts...');
-    Highcharts.chart('chart-duration', Highcharts.merge(darkTheme, {
+
+    Highcharts.chart('chart-alerts', Highcharts.merge(chartTheme, {
         chart: {
             type: 'column'
         },
         title: {
-            text: 'Call Duration (by minute)'
-        },
-        subtitle: {
-            text: data.date
+            text: 'Alerts (per day)'
         },
         xAxis: {
-            categories: data.duration.minutes
+            categories: data.days
         },
         yAxis: {
             min: 0,
             title: {
-                text: 'Total calls'
+                text: 'Total alerts'
             }
         },
         series: [
             {
-                name: 'Calls',
-                data: data.duration.totals,
-                color: '#FFF'
+                name: 'Alerts',
+                data: data.totals,
+                color: '#E0ECFD'
             }
         ]
     }));
-    Highcharts.chart('chart-hour', Highcharts.merge(darkTheme, {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Call Volume (By hour)'
-        },
-        subtitle: {
-            text: data.date
-        },
-        xAxis: {
-            categories: data.totals.hour
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Total calls'
-            }
-        },
-        series: [
-            {
-                name: 'Calls',
-                data: data.totals.totals,
-                color: '#FFF'
-            }
-        ]
-    })); 
 }
 
-const darkTheme = {
+const chartTheme = {
     chart: {
-        backgroundColor: '#293a42'
+        backgroundColor: '#000030'
     },
     title: {
-        style: { color: '#ffffff' }
+        style: { color: '#E0ECFD' }
     },
     subtitle: {
         style: { color: '#cccccc' }
     },
     xAxis: {
-        labels: { style: { color: '#ffffff' } },
+        labels: { style: { color: '#E0ECFD' } },
         lineColor: '#37474F',
         tickColor: '#37474F'
     },
     yAxis: {
-        title: { style: { color: '#ffffff' } },
-        labels: { style: { color: '#ffffff' } },
+        title: { style: { color: '#E0ECFD' } },
+        labels: { style: { color: '#E0ECFD' } },
         gridLineColor: '#37474F'
     },
     legend: {
-        itemStyle: { color: '#ffffff' }
+        itemStyle: { color: '#E0ECFD' }
     }
 };
